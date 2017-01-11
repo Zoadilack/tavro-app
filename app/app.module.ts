e0cd45d54ca07ef80eb33e22eff7879a41406891
Hutch White <hutch@Hutchs-MacBook-Pro.local>
2017-01-10 21:32:29 -0800

    rebasing entire solution; this should be an easier set up

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { routes } from './app.routing';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    LoginComponent
+  ]
+})
+export class AppModule {}
---
+++ app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+}
