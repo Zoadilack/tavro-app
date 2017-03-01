b60f55f6751336a7aa145bf88388ce566b8033ec
Hutch White <hutchjwhite@gmail.com>
2017-02-28 22:17:13 -0800

    check for auth on home

--- app/home/containers/home.component.ts
+++ app/home/containers/home.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { UserService } from '../../services/user.service';
+import { AuthenticationService } from '../../services/auth.service';
+import { AlertService } from '../../services/alert.service';
+
+import { User } from '../../models/user.model';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-}
+export class HomeComponent implements OnInit {
+    public loading: boolean = false;
+
+    constructor( 
+        private userService: UserService,
+        private router: Router,
+        private authService: AuthenticationService,
+        private alertService: AlertService,         
+    ) {
+    }
+
+    public ngOnInit() {
+        if (this.userService.isAuthed()) {
+            this.router.navigate(['/dashboard']);
+        }
+        else {
+            this.router.navigate(['/login']);
+        }
+    }
