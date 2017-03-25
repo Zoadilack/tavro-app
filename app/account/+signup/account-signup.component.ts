440036baf802825edfee34d21d39ff9b7247e8a2
Jake Litwicki <jake.litwicki@gmail.com>
2017-03-24 23:46:18 -0700

    stub out account signup page

--- app/account/+signup/account-signup.component.ts
+++ app/account/+signup/account-signup.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MaterialModule, MdDialog } from '@angular/material';
+
+import { Response } from '@angular/http';
+import { HttpService } from '../../services/http.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { UserService } from '../../services/user.service';
+import { AuthenticationService } from '../../services/auth.service';
+import { AlertService } from '../../services/alert.service';
+import global = require('../../services/globals');
+
+import { User } from '../../models/user.model';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'account-signup',
@@ -6,4 +20,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./account-signup.component.scss'],
 })
 export class AccountSignupComponent {
+
+    constructor( 
+        private userService: UserService,
+        private router: Router,
+        private authService: AuthenticationService,
+        private alertService: AlertService,
+        public dialog: MdDialog,      
+        private http: HttpService
+    ) { 
+        const accountTypes = this.getAccountTypes();
+        console.log(accountTypes);
+    }
+
+    public ngOnInit() {
+
+        if (!this.userService.isAuthed()) {
+            this.router.navigate(['/login']);
+        }
+
+    }
+
+    getAccountTypes() : Observable<Comment[]> {
+
+         // ...using get request
+         return this.http.get(global.api + 'account_types')
+                        // ...and calling .json() on the response to return data
+                         .map((res:Response) => res.json())
+                         //...errors if any
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+
+     }
+
 }
--- app/account/account.module.ts
+++ app/account/account.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { routeComponents, routeProviders, routes } from './account.routing';
 
@@ -10,6 +11,7 @@ import { AccountSignupComponent } from './+signup/account-signup.component';
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
+        MaterialModule
     ],
     providers: [
         ...routeProviders,
